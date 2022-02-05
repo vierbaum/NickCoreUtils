f181f1cfc188e53566c05f729300538f3b2400ae
nld <nick_daiber@riseup.net>
2022-02-05 01:01:48 +0100

    minor changes

--- dmenu.sh
+++ dmenu.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 sel=$(echo -e "SaarlandCalc" | dmenu -x 6 -y 6 -z 1354 -l 10)
-echo $sel
 case $sel in
     SaarlandCalc)
         ~/Documents/programming/NickCoreUtils/saarlandCalculator.sh $(xclip -o)
